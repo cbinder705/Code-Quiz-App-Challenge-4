dc8f1a200b310dc30af3c3214f7fce5a8b0d273e
Cory Bee <cbinder705@gmail.com>
2023-01-25 15:25:30 -0500

    added homepage and highscores html files

--- script.js
+++ script.js
@@ -1,46 +1,50 @@
 var questionDataBase = [
   {
-question: "JavaScript is one of the few languages that run in a ____________",
-a: "Smart Phone",
-b: "Dumb Phone",
-c: "Web Browser",
-D: "Marathon",
-correct: "c",
+    question:
+      "JavaScript is one of the few languages that run in a ____________",
+    a: "Smart Phone",
+    b: "Dumb Phone",
+    c: "Web Browser",
+    D: "Marathon",
+    correct: "c",
   },
 
   {
-  question: "What does API stand for? ",
-a: "Apple Product Instruction",
-b: "Application Programming Interface",
-c: "Application Programming Interference",
-D: "Apple Pie Installation",
-correct: "b",
+    question: "What does API stand for? ",
+    a: "Apple Product Instruction",
+    b: "Application Programming Interface",
+    c: "Application Programming Interference",
+    D: "Apple Pie Installation",
+    correct: "b",
   },
 
   {
     question: "What does CSS stand for? ",
-  a: "Cascading Style Sheets",
-  b: "Cascading Style Software",
-  c: "Correct Color Scheme",
-  D: "Color Correcting Software",
-  correct: "a",
-    },
+    a: "Cascading Style Sheets",
+    b: "Cascading Style Software",
+    c: "Correct Color Scheme",
+    D: "Color Correcting Software",
+    correct: "a",
+  },
 
-    {
-      question: "Inside of which element do we place the link to our Javascript file? ",
+  {
+    question:
+      "Inside of which element do we place the link to our Javascript file? ",
     a: "<javascript>",
     b: "<JSCRIPT>",
     c: "<sscript>",
     D: "<script>",
     correct: "d",
-      },
+  },
 
-      {
-        question: "The condition in an if/else statement is enclosed with ___________ ",
-      a: "quotes",
-      b: "parenthesis",
-      c: "curly brackets",
-      D: "square brackers",
-      correct: "b",
-        },
+  {
+    question:
+      "The condition in an if/else statement is enclosed with ___________ ",
+    a: "quotes",
+    b: "parenthesis",
+    c: "curly brackets",
+    D: "square brackets",
+    correct: "b",
+  },
 ];
+var timer = document;
