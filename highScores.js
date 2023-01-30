a2591d999eeda2d069a128223d01db57c194a1fc
Cory Bee <cbinder705@gmail.com>
2023-01-30 14:10:22 -0500

    updated file structure

--- highScores.js
+++ highScores.js
@@ -1,12 +1,13 @@
 var highScore = document.querySelector("#highScore");
 var goBack = document.querySelector("#goBack");
-var allScores = localStorage.getItem("allScores"); // saves scores to local
-allScores = JSON.parse(allScores);
+var getAllScores = localStorage.getItem("allScores"); // saves scores to local
+getAllScores = JSON.parse(getAllScores);
 
-if (allScores !== null) {
-  for (var i = 0; i < allScores.length; i++) {
+if (getAllScores !== null) {
+  for (var i = 0; i < getAllScores.length; i++) {
     var createLi = document.createElement("li");
-    createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+    createLi.textContent =
+      getAllScores[i].initials + " " + getAllScores[i].score;
     highScore.appendChild(createLi);
   }
 }
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var score = 0;
 var questionDatabase = [
   {
-    title: "JavaScript is one of the few languages that run in a ____________",
+    name: "JavaScript is one of the few languages that run in a ____________",
     choices: ["Smart Phone", "Dumb Phone", "Web Browser", "Marathon"],
     answer: "Web Browser",
   },
 
   {
-    title: "What does API stand for? ",
+    name: "What does API stand for? ",
     choices: [
       "Apple Product Instruction",
       "Application Programming Interface",
@@ -18,7 +18,7 @@ var questionDatabase = [
   },
 
   {
-    title: "What does CSS stand for? ",
+    name: "What does CSS stand for? ",
     choices: [
       "Cascading Style Sheets",
       "Cascading Style Software",
@@ -29,21 +29,18 @@ var questionDatabase = [
   },
 
   {
-    title:
-      "Inside of which element do we place the link to our Javascript file? ",
+    name: "Inside of which element do we place the link to our Javascript file? ",
     choices: ["<javascript>", "<sscript>", "<JSCRIPT>", "<script>"],
     answer: "<script>",
   },
 
   {
-    title:
-      "The condition in an if/else statement is enclosed with ___________ ",
+    name: "The condition in an if/else statement is enclosed with ___________ ",
     choices: ["quotes", "parenthesis", "curly brackets", "square brackets"],
     answer: "parenthesis",
   },
 ];
 
-var score = 0;
 var questionIndex = 0; // keeps track of which question the user is on
 var currentTime = document.querySelector("#currentTime");
 var timer = document.querySelector("#startTime");
@@ -69,12 +66,12 @@ timer.addEventListener("click", function () {
   }
   render(questionIndex);
 });
-
+// generates quiz
 function render(questionIndex) {
   questionsContainer.innerHTML = "";
   ulCreate.innerHTML = "";
   for (var i = 0; i < questionDatabase.length; i++) {
-    var userQuestion = questionDatabase[questionIndex].title;
+    var userQuestion = questionDatabase[questionIndex].name;
     var userChoices = questionDatabase[questionIndex].choices;
     questionsContainer.textContent = userQuestion;
   }
@@ -84,5 +81,92 @@ function render(questionIndex) {
     questionsContainer.appendChild(ulCreate);
     ulCreate.appendChild(listItem);
     listItem.addEventListener("click", compare);
+  });
+}
+function compare(event) {
+  var element = event.target;
+  if (element.matches("li")) {
+    var createDiv = document.createElement("div");
+    createDiv.setAttribute("id", "createDiv");
+    if (element.textContent == questionDatabase[questionIndex].answer) {
+      score++;
+      createDiv.textContent = "You're Right!";
+    } else {
+      secondsLeft = secondsLeft - penalty;
+      createDiv.textContent = "Nope!";
+    }
+  }
+  questionIndex++;
+
+  if (questionIndex >= questionDatabase.length) {
+    gameOver();
+    createDiv.textContent =
+      "You answered " +
+      score +
+      "/" +
+      questionDatabase.length +
+      " questions correctly.";
+  } else {
+    render(questionIndex);
+  }
+  questionsContainer.appendChild(createDiv);
+}
+function gameOver() {
+  questionsContainer.innerHTML = "";
+  currentTime.innerHTML = "";
+  var createH1 = document.createElement("h1");
+  createH1.setAttribute("id", "createH1");
+  createH1.textContent = "GameOver!";
+  questionsContainer.appendChild(createH1);
+  var createP = document.createElement("p");
+  createP.setAttribute("id", "createP");
+  questionsContainer.appendChild(createP);
+
+  if (secondsLeft >= 0) {
+    var timeRemaining = secondsLeft;
+    var createP2 = document.createElement("p");
+    clearInterval(holdInterval);
+    createP.textContent = "Your final score is: " + timeRemaining;
+
+    questionsContainer.appendChild(createP2);
+  }
+
+  var createLabel = document.createElement("label");
+  createLabel.setAttribute("id", "createLabel");
+  createLabel.textContent = " Please enter your initials: ";
+
+  questionsContainer.appendChild(createLabel);
+
+  var createInput = document.createElement("input"); // prompts input for user initials
+  createInput.setAttribute("type", "text");
+  createInput.setAttribute("id", "initials");
+  createInput.textContent = "";
+
+  questionsContainer.appendChild(createInput);
+
+  var createSubmit = document.createElement("button");
+  createSubmit.setAttribute("type", "submit");
+  createSubmit.setAttribute("id", "Submit"); // generates submit button to push scores
+  createSubmit.textContent = "Submit";
+
+  questionsContainer.appendChild(createSubmit);
+
+  createSubmit.addEventListener("click", function () {
+    var initials = createInput.value;
+
+    var finalScore = {
+      initials: initials,
+      score: timeRemaining,
+    };
+    var allScores = localStorage.getItem("allScores"); // handles storage of user scores
+    if (allScores === null) {
+      allScores = [];
+    } else {
+      allScores = JSON.parse(allScores);
+    }
+    allScores.push(finalScore);
+    var newScore = JSON.stringify(allScores);
+    localStorage.setItem("allScores", newScore);
+    window.location.replace("./highScores.html");
   });
 }
